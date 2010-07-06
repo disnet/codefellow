0ae555a388fdc9107f30ebb2994e6e60131774f1
Marc Weber <marco-oweber@gmx.de>
2010-07-06 02:44:33 +0200

    cleanup code: - there is IterableLike's find - remove imports

--- codefellow-core/src/main/scala/de/tuxed/codefellow/moduleRegistry.scala
+++ codefellow-core/src/main/scala/de/tuxed/codefellow/moduleRegistry.scala
@@ -9,14 +9,8 @@ import scala.actors.remote.RemoteActor
 
 import scala.collection.mutable.ListBuffer
 
-import scala.tools.nsc.interactive.{Global, CompilerControl}
-import scala.tools.nsc.{Settings, FatalError}
-import scala.tools.nsc.reporters.{Reporter, ConsoleReporter}
-import scala.tools.nsc.util.{ClassPath, MergedClassPath, SourceFile, Position, OffsetPosition, NoPosition}
 import scala.collection.mutable.{ HashMap, HashEntry, HashSet }
 import scala.collection.mutable.{ ArrayBuffer, SynchronizedMap,LinkedHashMap }
-import scala.tools.nsc.symtab.Types
-import scala.tools.nsc.symtab.Flags
 
 
 case class Request(moduleIdentifierFile: String, message: AnyRef)
@@ -24,31 +18,31 @@ case class Request(moduleIdentifierFile: String, message: AnyRef)
 class ModuleRegistry(modules: List[Module]) extends Actor {
 
   def act {
-    modules foreach { m =>
-      println("Module: " + m.name + " in directory: " + m.path)
-      m.start()
-    }
-    loop {
-      try {
-        receive {
-          case Request(moduleIdentifierFile, message) => {
-            val selected = modules.filter(m => moduleIdentifierFile.startsWith(m.path))
-            if (selected.size != 0) {
-              val result = selected(0) !? message
-              sender ! Right(result)
-            } else {
-              throw new Exception("File [" + moduleIdentifierFile + "] not part of the current project!")
+  modules foreach { m =>
+    println("Module: " + m.name + " in directory: " + m.path)
+    m.start()
+  }
+  loop {
+    receive {
+      case Request(moduleIdentifierFile, message) => {
+
+        sender ! {
+          try {
+            modules.find(m => moduleIdentifierFile.startsWith(m.path)) match {
+              case Some(module) => Right(module !? message)
+              case None => Left("File [" + moduleIdentifierFile + "] not part of the current project!")
+            }
+          } catch {
+            case e: Exception => {
+              e.printStackTrace
+              sender ! Left("Exception :"+e.getMessage+"\n"+e.getStackTraceString)
             }
           }
         }
-      } catch {
-        case e: Exception => {
-          e.printStackTrace
-          sender ! Left("Exception :"+e.getMessage+"\n"+e.getStackTraceString)
-        }
-      }
-    }
-  }
+
+      } // case
+    } // receive
+  } // loop
+  } // act
   
 }
-
